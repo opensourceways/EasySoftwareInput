d608f00eba79ee237e3f0f9a93b0a74d2c89ffd5
gouzhonglin <gouzhonglin@huawei.com>
2024-02-26 15:08:24 +0800

    parse rpm package of /repodata/primary.xml and write to mysql

---
+++ parse-repodata-primary-xml/src/main/java/com/example/service/ExecuteService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.entity.po.RPMPackage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class ExecuteService {
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Value("post.url")
+    private String postUrl;
+
+    
+    public String insertRPMPackage(RPMPackage pkg) {
+        String jsonPkg = "";
+        try {
+            jsonPkg= objectMapper.writeValueAsString(pkg);
+        } catch (JsonProcessingException e) {
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(jsonPkg, headers);
+
+
+        return restTemplate.postForObject(postUrl, request, String.class);
+    }
+}
