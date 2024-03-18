eb4199c16c039e84456420c695244910aec9cba5
gouzhonglin <gouzhonglin@huawei.com>
2024-03-18 10:14:11 +0800

    rpm

---
+++ one-server/easysoftwareinput/src/main/java/com/easysoftwareinput/application/sig/SigService.java
@@ -0,0 +1,8 @@
+package com.easysoftwareinput.application.sig;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SigService {
+    
+}
---
+++ one-server/easysoftwareinput/src/main/java/com/easysoftwareinput/common/utils/MySqlInjector.java
@@ -0,0 +1,20 @@
+package com.easysoftwareinput.common.utils;
+
+import java.util.List;
+
+import com.baomidou.mybatisplus.core.injector.AbstractMethod;
+import com.baomidou.mybatisplus.core.injector.DefaultSqlInjector;
+import com.baomidou.mybatisplus.core.metadata.TableInfo;
+import com.baomidou.mybatisplus.extension.injector.methods.InsertBatchSomeColumn;
+
+public class MySqlInjector extends DefaultSqlInjector {
+
+    @Override
+    public List<AbstractMethod> getMethodList(Class<?> mapperClass, TableInfo tableInfo) {
+        // 防止父类方法不可用
+        List<AbstractMethod> methodList = super.getMethodList(mapperClass, tableInfo);
+        methodList.add(new InsertBatchSomeColumn());
+        return methodList;
+    }
+}
+
