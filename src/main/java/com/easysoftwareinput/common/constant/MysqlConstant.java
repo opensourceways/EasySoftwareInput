0a1ed5da11a28fa6de518c2b42fea9b4d44c44f8
gi1tee <gouzhonglin@huawei.com>
2024-06-03 17:00:47 +0800

    checkstyle

--- src/main/java/com/easysoftwareinput/common/constant/MysqlConstant.java
+++ src/main/java/com/easysoftwareinput/common/constant/MysqlConstant.java
@@ -1,5 +1,14 @@
 package com.easysoftwareinput.common.constant;
 
-public class MysqlConstant {
+public final class MysqlConstant {
+    // Private constructor to prevent instantiation of the utility class
+    private MysqlConstant() {
+        // private constructor to hide the implicit public one
+        throw new AssertionError("ClientUtil class cannot be instantiated.");
+    }
+
+    /**
+     * batch length.
+     */
     public static final int BATCH_LENGHT = 5000;
 }
