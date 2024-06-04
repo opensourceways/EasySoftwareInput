41e2440056d451c300d7cecd6b2152bcc5a1bb10
gi1tee <gouzhonglin@huawei.com>
2024-06-04 10:43:06 +0800

    checkstyle

--- src/main/java/com/easysoftwareinput/infrastructure/BasePackageDO.java
+++ src/main/java/com/easysoftwareinput/infrastructure/BasePackageDO.java
@@ -8,20 +8,39 @@ import lombok.Data;
 @Data
 @TableName("base_package_info")
 public class BasePackageDO {
-    
+    /**
+     * name of pkg.
+     */
     @TableId
     private String name;
-    
+
+    /**
+     * category of pkg.
+     */
     private String category;
-    
+
+    /**
+     * maintianer of pkg.
+     */
     private String maintainerId;
-    
+
+    /**
+     * maintainer email of pkg.
+     */
     private String maintainerEmail;
-    
+
+    /**
+     * maintainer gitee id of pkg.
+     */
     private String maintainerGiteeId;
-    
+
+    /**
+     * maintianer update at of pkg.
+     */
     private String maintainerUpdateAt;
-    
+
+    /**
+     * download count of pkg.
+     */
     private String downloadCount;
-    
 }
