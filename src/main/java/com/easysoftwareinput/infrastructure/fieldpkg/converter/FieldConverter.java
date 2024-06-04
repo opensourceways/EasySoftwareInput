e00d7952ec7f25dce3cd5fb4e346c1c68f26b1cc
gi1tee <gouzhonglin@huawei.com>
2024-06-04 10:24:08 +0800

    checkstyle

--- src/main/java/com/easysoftwareinput/infrastructure/fieldpkg/converter/FieldConverter.java
+++ src/main/java/com/easysoftwareinput/infrastructure/fieldpkg/converter/FieldConverter.java
@@ -14,6 +14,11 @@ import com.easysoftwareinput.infrastructure.fieldpkg.dataobject.FieldDo;
 @Component
 public class FieldConverter {
 
+    /**
+     * convert pkg to data object.
+     * @param fList list of pkg.
+     * @return list of data object.
+     */
     public List<FieldDo> toDo(List<Field> fList) {
         List<FieldDo> dList = new ArrayList<>();
         for (Field f : fList) {
@@ -22,6 +27,11 @@ public class FieldConverter {
         return dList;
     }
 
+    /**
+     * convert pkg to data object.
+     * @param f pkg.
+     * @return data object.
+     */
     public FieldDo toDo(Field f) {
         FieldDo d = new FieldDo();
         BeanUtils.copyProperties(f, d);
@@ -30,5 +40,4 @@ public class FieldConverter {
         d.setUpdateAt(new Timestamp(System.currentTimeMillis()));
         return d;
     }
-    
 }
