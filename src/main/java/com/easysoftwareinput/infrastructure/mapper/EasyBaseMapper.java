c79452b133ddbb09043ce8c6270ab8ec65ff5b94
gi1tee <gouzhonglin@huawei.com>
2024-06-04 10:30:26 +0800

    checkstyle

--- src/main/java/com/easysoftwareinput/infrastructure/mapper/EasyBaseMapper.java
+++ src/main/java/com/easysoftwareinput/infrastructure/mapper/EasyBaseMapper.java
@@ -5,5 +5,10 @@ import java.util.List;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 public interface EasyBaseMapper<T> extends BaseMapper<T> {
+    /**
+     * insert batch.
+     * @param entityList lsit of pkg.
+     * @return the number of inserted.
+     */
     Integer insertBatchSomeColumn(List<T> entityList);
 }
