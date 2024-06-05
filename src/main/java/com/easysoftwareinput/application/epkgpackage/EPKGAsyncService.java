039ce47bb0f155dff320ed419ce3e718b4ee0dcb
gi1tee <gouzhonglin@huawei.com>
2024-06-05 09:05:47 +0800

    bug

--- src/main/java/com/easysoftwareinput/application/epkgpackage/EPKGAsyncService.java
+++ src/main/java/com/easysoftwareinput/application/epkgpackage/EPKGAsyncService.java
@@ -11,7 +11,11 @@
 
 package com.easysoftwareinput.application.epkgpackage;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.dom4j.Element;
 import org.slf4j.Logger;
@@ -27,6 +31,7 @@ import org.springframework.web.client.RestTemplate;
 import com.easysoftwareinput.application.rpmpackage.PkgService;
 import com.easysoftwareinput.domain.epkgpackage.ability.EPKGPackageConverter;
 import com.easysoftwareinput.domain.epkgpackage.model.EPKGPackage;
+import com.easysoftwareinput.infrastructure.epkgpkg.EpkgGatewayImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -43,6 +48,12 @@ public class EPKGAsyncService {
     private PkgService pkgService;
 
     /**
+     * gateway.
+     */
+    @Autowired
+    private EpkgGatewayImpl gateway;
+
+    /**
      * epkg pkg converter.
      */
     @Autowired
@@ -57,29 +68,26 @@ public class EPKGAsyncService {
      * @param srcMap src pkg name and url.
      */
     @Async("epkgasyncServiceExecutor")
-    public void executeAsync(Element e, Map<String, String> osMes, int i, String postUrl, Map<String, String> srcMap) {
-        LOGGER.info("thread name: {},  index: {}", Thread.currentThread().getName(), i);
-        Map<String, String> res = pkgService.parsePkg(e, osMes);
+    public void executeAsync(List<Element> eList, Map<String, String> osMes, int i, String postUrl,
+            Map<String, String> srcMap) {
+        List<EPKGPackage> pkgList = new ArrayList<>(eList.size());
+        Set<String> pkgIds = new HashSet<>();
 
-        EPKGPackage ePkg = epkgPackageConverter.toEntity(res, srcMap);
+        for (Element e : eList) {
+            Map<String, String> res = pkgService.parsePkg(e, osMes);
 
-        // 不添加源码包
-        if ("src".equals(ePkg.getArch())) {
-            return;
+            EPKGPackage ePkg = epkgPackageConverter.toEntity(res, srcMap);
+    
+            // 不添加源码包
+            if ("src".equals(ePkg.getArch())) {
+                continue;
+            }
+
+            // 舍弃主键重复的数据
+            if (pkgIds.add(ePkg.getPkgId())) {
+                pkgList.add(ePkg);
+            }
         }
-
-        String body = "";
-        ObjectMapper ma = new ObjectMapper();
-        try {
-            body = ma.writeValueAsString(ePkg);
-        } catch (Exception ex) {
-            LOGGER.info("can not tojson, pkg: {}", ePkg);
-        }
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.postForObject(postUrl, request, String.class);
+        gateway.saveAll(pkgList);
     }
 }
--- src/main/java/com/easysoftwareinput/infrastructure/epkgpkg/EpkgGatewayImpl.java
+++ src/main/java/com/easysoftwareinput/infrastructure/epkgpkg/EpkgGatewayImpl.java
@@ -13,21 +13,105 @@ package com.easysoftwareinput.infrastructure.epkgpkg;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.easysoftwareinput.domain.epkgpackage.model.EPKGPackage;
+import com.easysoftwareinput.infrastructure.epkgpkg.converter.EpkgConverter;
 import com.easysoftwareinput.infrastructure.epkgpkg.dataobject.EpkgDo;
 import com.easysoftwareinput.infrastructure.mapper.EpkgDoMapper;
 
 @Component
-public class EpkgGatewayImpl {
+public class EpkgGatewayImpl extends ServiceImpl<EpkgDoMapper, EpkgDo> {
+    /**
+     * logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(EpkgGatewayImpl.class);
+
     /**
      * mapper.
      */
     @Autowired
     private EpkgDoMapper mapper;
+
+    /**
+     * converter.
+     */
+    @Autowired
+    private EpkgConverter converter;
+
+    /**
+     * save pkg to database.
+     * @param epkgList list of pkgs.
+     * @return boolean.
+     */
+    public boolean saveAll(List<EPKGPackage> epkgList) {
+        List<EpkgDo> dList = converter.toDo(epkgList);
+
+        List<EpkgDo> existed = new ArrayList<>();
+        List<EpkgDo> unexisted = new ArrayList<>();
+        fillListById(dList, existed, unexisted);
+        return synSave(existed, unexisted);
+    }
+
+    /**
+     * divide dlist to existed and unexisted.
+     * @param dList dlist.
+     * @param existed existed.
+     * @param unexisted unexisted.
+     */
+    public void fillListById(List<EpkgDo> dList, List<EpkgDo> existed, List<EpkgDo> unexisted) {
+        Set<String> existedPkgIdSet = getExistedIds();
+        Map<Boolean, List<EpkgDo>> map = dList.stream()
+                .collect(Collectors.partitioningBy(d -> existedPkgIdSet.contains(d.getPkgId())));
+        existed.addAll(map.get(true));
+        unexisted.addAll(map.get(false));
+    }
+
+    /**
+     * get existed pkgids from table.
+     * @return list of pkgids.
+     */
+    public Set<String> getExistedIds() {
+        QueryWrapper<EpkgDo> wrapper = new QueryWrapper<>();
+        wrapper.select("distinct (pkg_id)");
+        List<EpkgDo> list = mapper.selectList(wrapper);
+        return list.stream().map(EpkgDo::getPkgId).collect(Collectors.toSet());
+    }
+
+    /**
+     * if existed, update row, if unexisted, insert row.
+     * @param existed existed.
+     * @param unexisted unexisted.
+     * @return boolean.
+     */
+    public synchronized boolean synSave(List<EpkgDo> existed, List<EpkgDo> unexisted) {
+        boolean inserted = false;
+        try {
+            inserted = saveBatch(unexisted, 1000);
+        } catch (Exception e) {
+            LOGGER.error("fail-to-write, e: {}", e.getMessage());
+        }
+
+        boolean updated = false;
+        try {
+            updated = updateBatchById(existed);
+        } catch (Exception e) {
+            LOGGER.error("fail-to-update, e: {}", e.getMessage());
+        }
+        if (inserted && updated) {
+            return true;
+        }
+        return false;
+    }
 
     /**
      * get distinct os from table.
